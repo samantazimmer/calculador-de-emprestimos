17a770257d21133d704ee6279ef5e9689c5900a4
samantazimmer <95974603+samantazimmer@users.noreply.github.com>
2022-02-12 15:13:50 -0300

    Adicionando Juros Veiculo

--- financiamentoVeiculo.py
+++ financiamentoVeiculo.py
@@ -1,3 +1,9 @@
+def calculaValorEmprestimo(valor):
+    return (valor * 10 / 100) + valor
+
+def printProsseguirComProcesso(nome, cpf):
+    print(f'Prosseguir com o processo para o cliente {nome}, portador do cpf {cpf}.')
+
 def processaFinanciamentoVeiculo(nome, cpf, dataNascimento, salario, cep, quantidadeAnos):
     
     possuiCnh = input("Possui CNH? \n S - sim \n N - não \n")
@@ -8,32 +14,32 @@ def processaFinanciamentoVeiculo(nome, cpf, dataNascimento, salario, cep, quanti
         possuiCnh == False
     
     if possuiCnh == True and salario <= 2000:
-        print(f'Prosseguir com o processo para o cliente {nome} portador do cpf {cpf}.')
+        printProsseguirComProcesso(nome, cpf)
         print("valor liberado foi R$ 40000")
         parcelas = quantidadeAnos * 12
-        valorParcela = 40000 / parcelas
+        valorParcela = calculaValorEmprestimo(40000) / parcelas
         print(f'Parcelas: {parcelas}x R${valorParcela}')
     
     if possuiCnh == True and salario > 2000 and salario <= 5000:
-        print(f'Prosseguir com o processo para o cliente {nome} portador do cpf {cpf}.')
+        printProsseguirComProcesso(nome, cpf)
         print("valor liberado foi R$ 80000")
         parcelas = quantidadeAnos * 12
-        valorParcela = 80000 / parcelas
+        valorParcela = calculaValorEmprestimo(80000) / parcelas
         print(f'Parcelas: {parcelas}x R${valorParcela}')
         
     if possuiCnh == True and salario > 5000 and salario <= 10000:
-        print(f'Prosseguir com o processo para o cliente {nome} portador do cpf {cpf}.')
+        printProsseguirComProcesso(nome, cpf)
         print("valor liberado foi R$ 120000")
         parcelas = quantidadeAnos * 12
-        valorParcela = 120000 / parcelas
+        valorParcela = calculaValorEmprestimo(120000) / parcelas
         print(f'Parcelas: {parcelas}x R${valorParcela}')
         
     if possuiCnh == True and salario > 10000:
-        print(f'Prosseguir com o processo para o cliente {nome} portador do cpf {cpf}.')
+        printProsseguirComProcesso(nome, cpf)
         print("valor liberado foi R$ 150000")
         parcelas = quantidadeAnos * 12
-        valorParcela = 150000 / parcelas
+        valorParcela = calculaValorEmprestimo(150000) / parcelas
         print(f'Parcelas: {parcelas}x R${valorParcela}')
         
     else:
-        print("Financiamento não liberado.")
+        print(f'Financiamento não liberado {nome}, portador do cpf {cpf}.')
